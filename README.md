b6977c6d66fb947809ad88b39d6b397200f2a26c
Abhinav Chennubhotla <51199011+PhoenixFlame101@users.noreply.github.com>
2023-04-25 16:20:05 +0530

    patient zero

---
+++ README.md
@@ -0,0 +1,5 @@
+# Web Server Analytics
+
+This is a simple python project that monitors a server and displays analytics.
+
+##### This project is maintained by [Abhinav Chennubhotla](https://github.com/PhoenixFlame101).
---
+++ server.py
@@ -0,0 +1,9 @@
+import os
+
+from http.server import HTTPServer, CGIHTTPRequestHandler
+os.chdir('../')
+
+server_object = HTTPServer(server_address=(
+    '', 8080), RequestHandlerClass=CGIHTTPRequestHandler)
+
+server_object.serve_forever()
