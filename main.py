c2d8174c9509e4bd1a5fc90fba74d42bfe2c9977
Abhinav Chennubhotla <51199011+PhoenixFlame101@users.noreply.github.com>
2023-04-27 09:53:04 +0530

    Add image page and fixed traffic metric

--- main.py
+++ main.py
@@ -1,10 +1,32 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, g, request
 import psutil
 import matplotlib.pyplot as plt
 import io
 import base64
+import os
 
 app = Flask(__name__)
+b_sent = 0
+b_recv = 0
+
+
+@app.before_request
+def before_request():
+    # g.bytes_received = 0
+    # g.bytes_sent = 0
+    pass
+
+
+@app.after_request
+def after_request(response):
+    global b_sent
+    global b_recv
+
+    response.direct_passthrough = False
+    b_sent += request.content_length or 0
+    b_recv += len(response.data)
+    # print(b_sent, b_recv)
+    return response
 
 
 @app.route('/')
@@ -16,9 +38,9 @@ def index():
     memory_usage = psutil.virtual_memory().percent
 
     # Get the bytes sent and received
-    net_io_counters = psutil.net_io_counters()
-    bytes_sent = net_io_counters.bytes_sent
-    bytes_recv = net_io_counters.bytes_recv
+    # net_io_counters = psutil.net_io_counters()
+    bytes_sent = b_sent//8
+    bytes_recv = b_recv//8
 
     # Create a graph of the CPU usage and memory usage
     fig, ax = plt.subplots()
@@ -38,6 +60,10 @@ def index():
     # Render the template with the statistics and graph
     return render_template('index.html', cpu_usage=cpu_usage, memory_usage=memory_usage, bytes_sent=bytes_sent, bytes_recv=bytes_recv, graph=graph)
 
+
+@app.route('/image')
+def image():
+    return render_template("image.html")
 
 if __name__ == '__main__':
     app.run()
